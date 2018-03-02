d1f8d3aac9e9b7b960d270f93ec64a822114c237
alejandrom <amesias@thoughtworks.com>
2018-03-02 16:20:51 -0500

    BookLister test red

---
+++ src/com/twu/biblioteca/BookLister.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+
+public class BookLister {
+    public static ArrayList<SingleBook> getAllBooks(){
+        ArrayList<SingleBook> allBooks = null;
+        return allBooks;
+    }
+}
