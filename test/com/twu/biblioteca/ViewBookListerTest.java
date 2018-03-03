aea53c689d466a284f3d7dacf0ac2c64b1215605
alejandrom <amesias@thoughtworks.com>
2018-03-02 22:18:18 -0500

    proof of concept for testing outputstreams

---
+++ test/com/twu/biblioteca/ViewBookListerTest.java
@@ -0,0 +1,31 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ViewBookListerTest {
+
+    private final ByteArrayOutputStream printedOutput = new ByteArrayOutputStream();
+
+    @Before
+    public void captureStream() {
+        System.setOut(new PrintStream(printedOutput));
+    }
+
+    @After
+    public void restoreStream() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void shouldDisplaytheBookList() {
+        System.out.print("content");
+        assertEquals("content", printedOutput.toString());
+    }
+}
