6e7d0d3bbc74b0d18c8c7845bc5bb59f88276046
alejandrom <amesias@thoughtworks.com>
2018-03-05 12:05:21 -0500

    Main Menu, MenuOption, red, MenuOption as Object instead of primitive

---
+++ src/com/twu/biblioteca/MenuOption.java
@@ -0,0 +1,24 @@
+package com.twu.biblioteca;
+
+public class MenuOption {
+
+    private String optionLabel;
+    private String optionId;
+
+    public MenuOption(String optionLabel, String optionId) {
+        this.optionLabel = optionLabel;
+        this.optionId = optionId;
+    }
+
+    public String getOptionLabel() {
+        //return optionLabel;
+        return "";
+    }
+
+    public String getOptionId() {
+        //return optionId;
+        return "";
+    }
+
+
+}
---
+++ test/com/twu/biblioteca/MenuOptionTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MenuOptionTest {
+
+    @Test
+    public void shouldHaveCreatedANewMenuOption() {
+        MenuOption anOption = new MenuOption("labelForOption", "X");
+
+        assertEquals("labelForOption", anOption.getOptionLabel());
+        assertEquals("X", anOption.getOptionId());
+    }
+}
