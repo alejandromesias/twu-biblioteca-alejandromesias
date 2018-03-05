d205d0504b432270859281e3f2cf13f258514fa9
alejandrom <amesias@thoughtworks.com>
2018-03-04 23:10:25 -0500

    Main Menu, test green, first option

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -3,7 +3,7 @@ package com.twu.biblioteca;
 
 public class MainMenu {
 
-    private static String[] options = {""};
+    private static String[] options = {"List Books"};
 
     public static String[] getOptions() {
         return options;
