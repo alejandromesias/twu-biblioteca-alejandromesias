7681ff1ce60830413a79f38715b592b33a15140d
alejandrom <amesias@thoughtworks.com>
2018-03-06 14:26:43 -0500

    CheckoutBook, BookManager, test lending of a book, green proper

--- src/com/twu/biblioteca/BookManager.java
+++ src/com/twu/biblioteca/BookManager.java
@@ -1,8 +1,22 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BookManager {
 
-    public static boolean lend(int bookId, String personName){
-        return true;
+    public static boolean lend(int requestedBookId, String personName){
+        ArrayList<SingleBook> allBooks = BookLister.getAllBooks();
+
+        for (SingleBook book : allBooks){
+            if (book.getBookId() == requestedBookId) {
+                SingleBook foundBook = book;
+
+                foundBook.setCheckedOutByPerson(personName);
+                foundBook.setIsCheckedOut(true);
+                return foundBook.getIsCheckedOut();
+            }
+        }
+
+        return false;
     }
 }
--- test/com/twu/biblioteca/BookManagerTest.java
+++ test/com/twu/biblioteca/BookManagerTest.java
@@ -7,10 +7,10 @@ import static junit.framework.TestCase.assertTrue;
 public class BookManagerTest {
     @Test
     public void shouldLendABookById() {
-        int bookId = 3;
+        int requestedBookId = 3;
         String personName = "Jane Doe";
 
-        Boolean wasLend = BookManager.lend(bookId,personName);
+        Boolean wasLend = BookManager.lend(requestedBookId,personName);
         assertTrue(wasLend);
     }
 }
