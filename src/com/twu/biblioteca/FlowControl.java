f23b722f948b08c68c8a69061d6dbba0ae50baa4
alejandrom <amesias@thoughtworks.com>
2018-03-06 09:24:53 -0500

    Main Menu, FlowControlTest, green, List Books

--- src/com/twu/biblioteca/FlowControl.java
+++ src/com/twu/biblioteca/FlowControl.java
@@ -5,7 +5,7 @@ public class FlowControl {
     //TODO: Replace this code smell with polymorphism
     public static void launch(String code){
         if (code == "List Books"){
-            //ViewBookLister.display();
+            ViewBookLister.display();
         }
     }
 }
